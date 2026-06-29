58233add08797e1b8cbf4faac2aab01cec8b7985
Alberto Perez <alberto.perez@impulsotecnologico.com.mx>
2026-06-29 11:12:29 -0600

    Update config.js

--- src/QuinielaMundial.Api/wwwroot/config.js
+++ src/QuinielaMundial.Api/wwwroot/config.js
@@ -1,5 +1,5 @@
 window.QUINIELA_CONFIG = {
-  apiBaseUrl: "https://quiniela-back-hqctdxdacdg5c4fq.mexicocentral-01.azurewebsites.net"
+  apiBaseUrl: "https://quiniela-back-hqctdxdacdg5c4fq.mexicocentral-01.azurewebsites.net",
   //apiBaseUrl: "http://localhost:5037",
   maintenanceMode: false,
   maintenanceTitle: "Quiniela en mantenimiento",
